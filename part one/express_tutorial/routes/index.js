8b6e78db44ea76c4f7b2a0a71566fec48eac9a4c
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2024-09-03 12:00:40 +0600

    axios_method

--- part one/express_tutorial/routes/index.js
+++ part one/express_tutorial/routes/index.js
@@ -3,9 +3,40 @@ var router = express.Router();
 const axios = require("axios")
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-  axios.get('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => res.render('index', { title: response.data.title }))
+  // axios.get('https://jsonplaceholder.typicode.com/todos/', {
+  //   params: { id: 5 }
+  // })
+  //   .then(response => {
+  //     console.log("this is get", response.data)
+  //     // res.send(response.data)
+  //   })
+  //   .catch(error => console.log(error))
+
+  // axios.post("https://jsonplaceholder.typicode.com/posts",
+  //   { title: "out Title", body: "thi is test body" }
+  // )
+  //   .then(response => {
+  //     // console.log(response.data)
+  //     res.send(response.data)
+  //   })
+  //   .catch(error => console.log(error))
+  // axios.patch("https://jsonplaceholder.typicode.com/posts/1",
+  //   { title: "out Title", body: "change post1 sfa" }
+  // )
+  //   .then(response => {
+  //     // console.log(response.data)
+  //     res.send(response.data)
+  //   })
+  //   .catch(error => console.log(error))
+
+  axios.delete("https://jsonplaceholder.typicode.com/posts/1"
+  )
+    .then(response => {
+      // console.log(response.data)
+      res.send(response.data)
+    })
     .catch(error => console.log(error))
+
 
 });
 
