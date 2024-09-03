202daed26333fd09db51773b79e97fd2551144fa
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2024-09-03 13:00:53 +0600

    complted

--- part one/express_tutorial/routes/index.js
+++ part one/express_tutorial/routes/index.js
@@ -3,40 +3,17 @@ var router = express.Router();
 const axios = require("axios")
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-  // axios.get('https://jsonplaceholder.typicode.com/todos/', {
-  //   params: { id: 5 }
-  // })
-  //   .then(response => {
-  //     console.log("this is get", response.data)
-  //     // res.send(response.data)
-  //   })
-  //   .catch(error => console.log(error))
-
-  // axios.post("https://jsonplaceholder.typicode.com/posts",
-  //   { title: "out Title", body: "thi is test body" }
-  // )
-  //   .then(response => {
-  //     // console.log(response.data)
-  //     res.send(response.data)
-  //   })
-  //   .catch(error => console.log(error))
-  // axios.patch("https://jsonplaceholder.typicode.com/posts/1",
-  //   { title: "out Title", body: "change post1 sfa" }
-  // )
-  //   .then(response => {
-  //     // console.log(response.data)
-  //     res.send(response.data)
-  //   })
-  //   .catch(error => console.log(error))
-
-  axios.delete("https://jsonplaceholder.typicode.com/posts/1"
-  )
-    .then(response => {
-      // console.log(response.data)
-      res.send(response.data)
+  axios.post('https://jsonplaceholder.typicode.com/todos/', {
+    title: "Learning post with Headers",
+    body: "Body with post Headers"
+  }, {
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then(resp => {
+      console.log(resp);
+      res.send("posted data whit heards");
     })
-    .catch(error => console.log(error))
-
+    .catch(e => console.log(e))
 
 });
 
